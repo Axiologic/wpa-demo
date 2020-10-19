9d0fa0cd7b68476c045095a06a0ff4bbe412f42e
Daniel Visoiu <daniel.visoiu@toptal.com>
2020-10-19 23:31:19 +0300

    Update sw config.

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  "globDirectory": ".",
-  "globPatterns": [
-    "**/*.md"
-  ],
-  "swDest": "sw.js"
+  globDirectory: ".",
+  globPatterns: ["**/*.md", "**/*.html"],
+  swDest: "sw.js",
 };
