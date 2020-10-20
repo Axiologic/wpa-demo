5433e930e291848c74877787013e3c86006b1bd2
Daniel Visoiu <daniel.visoiu@toptal.com>
2020-10-20 21:20:31 +0300

    Update

--- workbox-config.js
+++ workbox-config.js
@@ -1,10 +1,10 @@
 module.exports = {
   globDirectory: ".",
-  globPatterns: ["**/*.md", "**/*.html", "**/*.js", "**/*.css"],
+  globPatterns: ["**/*.md", "**/*.html", "**/*.js", "**/*.css", "**/*.png"],
   globIgnores: ["_layouts/**"],
   swDest: "sw.js",
   additionalManifestEntries: [
-    { url: "/wpa-demo/index.html", revision: "11" },
-    { url: "/wpa-demo/", revision: "11" },
+    { url: "/wpa-demo/index.html", revision: "14" },
+    { url: "/wpa-demo/", revision: "14" },
   ],
 };
