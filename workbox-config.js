ec3c20c498870c198af59d18ac4d33842d8d56e7
Daniel Visoiu <daniel.visoiu@toptal.com>
2020-10-20 17:20:31 +0300

    Update

--- workbox-config.js
+++ workbox-config.js
@@ -4,7 +4,7 @@ module.exports = {
   globIgnores: ["_layouts/**"],
   swDest: "sw.js",
   additionalManifestEntries: [
-    { url: "/wpa-demo/index.html", revision: "5" },
-    { url: "/wpa-demo/", revision: "5" },
+    { url: "/wpa-demo/index.html", revision: "6" },
+    { url: "/wpa-demo/", revision: "6" },
   ],
 };
