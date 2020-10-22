6cdf5cf0988f244037d3e683a85136b171a468ce
Daniel Visoiu <daniel.visoiu@toptal.com>
2020-10-22 14:28:55 +0300

    Update

--- loader-config.js
+++ loader-config.js
@@ -1,4 +1,4 @@
-import APP_CONFIG from './config-constants.js';
+import APP_CONFIG from "./config-constants.js";
 
 let linkElement = document.createElement("link");
 let theme = APP_CONFIG.THEME;
@@ -7,12 +7,11 @@ linkElement.type = "text/css";
 linkElement.rel = "stylesheet";
 document.head.appendChild(linkElement);
 
-
 if (APP_CONFIG.PLUGIN_SCRIPT) {
-	let scriptElement = document.createElement("script");
-	scriptElement.src = APP_CONFIG.PLUGIN_SCRIPT;
-	scriptElement.type = "module";
-	document.body.appendChild(scriptElement);
+  let scriptElement = document.createElement("script");
+  scriptElement.src = APP_CONFIG.PLUGIN_SCRIPT;
+  scriptElement.type = "module";
+  document.body.appendChild(scriptElement);
 }
 
 window.APP_CONFIG = APP_CONFIG;
--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   globDirectory: "./",
   globPatterns: ["**/*.{html,js,css,ttf,eot,svg,woff,woff2,jpg,png}"],
-  globIgnores: ["sw*.js", "node_modules/**/*.*"],
+  globIgnores: ["sw*.js", "node_modules/**/*.*", "loader-config.js"],
   swDest: "./swPwa.js",
   cleanupOutdatedCaches: true,
 };
