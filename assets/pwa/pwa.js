b7b144dbee732ecf843921d06e8f95df3784ef29
Alboaie Sînică <salboaie@gmail.com>
2020-10-22 15:57:31 +0300

    Update pwa.js

--- assets/pwa/pwa.js
+++ assets/pwa/pwa.js
@@ -2,7 +2,7 @@
 if ("serviceWorker" in navigator) {
   // Use the window load event to keep the page load performant
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("swPwa.js", { scope: "/wpa-demo/" });
+    navigator.serviceWorker.register("swPwa.js", { scope: "/wpa-demo1/" });
   });
 }
 
