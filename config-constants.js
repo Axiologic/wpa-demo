83ba8fa7059b983af3886d1e4bffa9618ff7db78
Daniel Visoiu <daniel.visoiu@toptal.com>
2020-10-25 15:30:54 +0200

    Update

--- config-constants.js
+++ config-constants.js
@@ -1,7 +1,7 @@
 let APP_CONFIG = {
   THEME: "app",
   LABELS_DICTIONARY: {
-    APP_NAME: "Web Dossier 15",
+    APP_NAME: "Web Dossier 16",
     APP_DESCRIPTION: "A smart application to keep your secrets",
     NEW_DOSSIER: "New Dossier",
     RESTORE_DOSSIER: "Restore Dossier",
